77b92a2a3814742bee425aa0d7712101a4b98b53
amodroid <amod.mulay@gmail.com>
2015-10-14 12:27:58 +0200

    added comment

--- src/test/java/com/avm/zoocode/ZooCodeApplicationTests.java
+++ src/test/java/com/avm/zoocode/ZooCodeApplicationTests.java
@@ -5,7 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-
+/**
+ * 
+ * @author amodm1
+ *
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = com.avm.zoocode.ZooplusCodeApplication.class)
 @WebAppConfiguration
